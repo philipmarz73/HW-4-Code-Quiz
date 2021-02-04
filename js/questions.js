a779c52bfdf5bdf5aa740a90bca9c1f8b4a50fb0
Phlipper <marzullop73@gmail.com>
2021-02-04 16:42:21 -0500

    added the rest of quiz questions

--- js/questions.js
+++ js/questions.js
@@ -1,27 +1,27 @@
 var questions = [
     {
-        title: ".addEventListener() takes in ___ arguments.",
+        title: "'.addEventListener()' takes in ___ arguments.",
         choices: ["none", "one", "two", "unlimited"],
         answer: "two"
     },
     {
-        title: ".addEventListener() takes in ___ arguments.",
-        choices: ["none", "one", "two", "unlimited"],
-        answer: "two"
+        title: "In HTML a '<button>' is a ___ element",
+        choices: ["syntax", "form", "string", "class"],
+        answer: "form"
     },
     {
-        title: ".addEventListener() takes in ___ arguments.",
-        choices: ["none", "one", "two", "unlimited"],
-        answer: "two"
+        title: "In CSS, every element rests within a series of ___.",
+        choices: ["parentheses", "rows", "formats", "boxes"],
+        answer: "boxes"
     },
     {
-        title: ".addEventListener() takes in ___ arguments.",
-        choices: ["none", "one", "two", "unlimited"],
-        answer: "two"
+        title: "What type of method is '.attr()'?",
+        choices: ["CSS", "jQuery", "Express", "AJAX"],
+        answer: "jQuery"
     },
     {
-        title: ".addEventListener() takes in ___ arguments.",
-        choices: ["none", "one", "two", "unlimited"],
-        answer: "two"
+        title: "Arrow functions should be avoided for object methods because they can change ___.",
+        choices: ["parents", "type", "degree", "scope"],
+        answer: "scope"
     }
 ];
